96713ab3c60ffab03ca1013e16d899ad44c9ca25
Felipe José Guimarães Fonseca <46244847+felipejoseguimaraesfonseca@users.noreply.github.com>
2021-11-26 16:15:26 -0300

    SplashActivity.kt code added, activity_navigation.xml and mobile_navigation.xml modified, UserEntity.kt added

--- app/src/main/java/com/example/videoapp/view/activities/SplashActivity.kt
+++ app/src/main/java/com/example/videoapp/view/activities/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.example.videoapp.view.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.videoapp.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, SignInOrSignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
