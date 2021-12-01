9b5a326503738bb1cbc7889582250f8e88f843d1
Felipe José Guimarães Fonseca <46244847+felipejoseguimaraesfonseca@users.noreply.github.com>
2021-12-01 19:04:49 -0300

    Sign Up code logic added in SingUpWithAnEmailActivity.kt. Sign In code logic added in SignInWithAnEmailActivity.kt.

--- app/src/main/java/com/example/videoapp/view/activities/SignInWithAnEmailActivity.kt
+++ app/src/main/java/com/example/videoapp/view/activities/SignInWithAnEmailActivity.kt
@@ -1,12 +1,78 @@
 package com.example.videoapp.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.videoapp.R
+import com.example.videoapp.databinding.ActivitySignInWithAnEmailBinding
+import com.example.videoapp.viewmodel.UserViewModel
 
-class SignInWithAnEmailActivity : AppCompatActivity() {
+class SignInWithAnEmailActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivitySignInWithAnEmailBinding
+    private lateinit var mViewModel: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in_with_an_email)
+        binding = ActivitySignInWithAnEmailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[UserViewModel::class.java]
+
+        observe()
+
+        setListeners()
     }
+
+    override fun onClick(view: View) {
+        val idClick = view.id
+
+        val email = binding.emailDarkOrangeEditText.text.toString()
+        val password = binding.passwordDarkOrangeEditText.text.toString()
+
+        if (idClick == R.id.cancel_white_button) {
+            val intent = Intent(this, SignInOrSignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else if (idClick == R.id.sign_in_dark_orange_button) {
+            if (email == "" && password == "") {
+                Toast.makeText(
+                    this, getString(R.string.fill_all_the_fields),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (email == "" || password == "") {
+                Toast.makeText(
+                    this, getString(R.string.fill_all_the_fields),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                val observer = observe()
+
+                if (observer.toString().toInt() == R.string.account_logged_successfully) {
+                    val intent = Intent(this, NavigationActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun observe() {
+        mViewModel.messageEventData.observe(this) { stringResId ->
+            Toast.makeText(this, stringResId, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setListeners() {
+        binding.cancelWhiteButton.setOnClickListener(this)
+        binding.signInDarkOrangeButton.setOnClickListener(this)
+    }
+
 }
--- app/src/main/java/com/example/videoapp/view/activities/SignUpWithAnEmailActivity.kt
+++ app/src/main/java/com/example/videoapp/view/activities/SignUpWithAnEmailActivity.kt
@@ -1,12 +1,120 @@
 package com.example.videoapp.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.videoapp.R
+import com.example.videoapp.databinding.ActivitySignUpWithAnEmailBinding
+import com.example.videoapp.viewmodel.UserViewModel
 
-class SignUpWithAnEmailActivity : AppCompatActivity() {
+class SignUpWithAnEmailActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivitySignUpWithAnEmailBinding
+
+    private lateinit var mViewModel: UserViewModel
+    private var mUserId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_with_an_email)
+        binding = ActivitySignUpWithAnEmailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        mViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[UserViewModel::class.java]
+
+        observe()
+
+        setListeners()
     }
+
+    override fun onClick(view: View) {
+        val idClick = view.id
+
+        val firstName = binding.fistNameDarkOrangeEditText.text.toString()
+        val lastName = binding.lastNameDarkOrangeEditText.text.toString()
+        val email = binding.emailDarkOrangeEditText.text.toString()
+        val confirmEmail = binding.confirmEmailDarkOrangeEditText.text.toString()
+        val password = binding.passwordDarkOrangeEditText.text.toString()
+        val confirmPassword = binding.confirmPasswordDarkOrangeEditText.text.toString()
+
+        if (idClick == R.id.cancel_white_button) {
+            val intent = Intent(this, SignInOrSignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else if (idClick == R.id.sign_up_dark_orange_button) {
+            if (
+                firstName == "" &&
+                lastName == "" &&
+                email == "" &&
+                confirmEmail == "" &&
+                password == "" &&
+                confirmPassword == ""
+            ) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.fill_all_the_fields),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (
+                firstName == "" ||
+                lastName == "" ||
+                email == "" ||
+                confirmEmail == "" ||
+                password == "" ||
+                confirmPassword == ""
+            ) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.fill_all_the_fields),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                if (email != confirmEmail && password != confirmPassword) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.are_not_the_same),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (email != confirmEmail || password != confirmPassword) {
+                    if (email != confirmEmail) {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.email_and_confirm_email_are_not_the_same),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else if (password != confirmPassword) {
+                        Toast.makeText(
+                            this,
+                            getString(R.string.password_and_confirm_password_are_not_the_same),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    mViewModel.saveUser(mUserId, firstName, lastName, email, password)
+                    val intent = Intent(this, SignUpWithAnEmailActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun observe() {
+        mViewModel.messageEventData.observe(this) { stringResId ->
+            Toast.makeText(this, stringResId, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setListeners() {
+        binding.cancelWhiteButton.setOnClickListener(this)
+        binding.signUpDarkOrangeButton.setOnClickListener(this)
+    }
+
 }
