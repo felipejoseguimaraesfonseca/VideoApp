39a45c02481cf3b05a897949dfe513f2432b0639
Felipe José Guimarães Fonseca <46244847+felipejoseguimaraesfonseca@users.noreply.github.com>
2021-12-02 18:59:32 -0300

    Boot mode function added in SplashActivty.kt

--- app/src/main/java/com/example/videoapp/view/activities/SplashActivity.kt
+++ app/src/main/java/com/example/videoapp/view/activities/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.example.videoapp.view.activities
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room.databaseBuilder
+import androidx.room.RoomDatabase
 import com.example.videoapp.R
+import com.example.videoapp.data.repository.AppDatabase
+import java.io.File
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -14,6 +20,22 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        bootMode()
+
+        splashTransaction()
+    }
+
+    private fun bootMode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val context = createDeviceProtectedStorageContext()
+
+            context.getSharedPreferences("DeviceProtectedStorage", Context.MODE_PRIVATE)
+            context.openFileOutput("sample.txt", MODE_PRIVATE)
+            RoomDatabase.databaseBuilder(context, AppDatabase::class.java, "app_database").build()
+        }
+    }
+
+    private fun splashTransaction() {
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashActivity, SignInOrSignUpActivity::class.java)
             startActivity(intent)
