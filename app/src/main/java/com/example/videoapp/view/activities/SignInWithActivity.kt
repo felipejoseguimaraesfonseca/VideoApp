104880b42ef3a6cab6269385997f408f9e02401a
Felipe José Guimarães Fonseca <46244847+felipejoseguimaraesfonseca@users.noreply.github.com>
2022-01-03 18:56:13 -0300

    Fixing some features.

--- app/src/main/java/com/example/videoapp/view/activities/SignInWithActivity.kt
+++ app/src/main/java/com/example/videoapp/view/activities/SignInWithActivity.kt
@@ -1,15 +1,11 @@
 package com.example.videoapp.view.activities
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.example.videoapp.R
 import com.example.videoapp.databinding.ActivitySignInWithBinding
 import com.facebook.AccessToken
@@ -35,69 +31,9 @@ abstract class SignInWithActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInWithBinding.inflate(layoutInflater)
         val view = binding.root
-        setTheme(R.style.Theme_VideoApp)
         setContentView(view)
 
-        requestPermissions()
         setListeners()
-    }
-
-    private fun hasReadExternalStoragePermission() =
-        ActivityCompat.checkSelfPermission(
-            this,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-
-    private fun hasManageOrWriteExternalStoragePermission() =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.MANAGE_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        } else {
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        }
-
-    private fun requestPermissions() {
-        val permissionToRequest = mutableListOf<String>()
-
-        if (!hasManageOrWriteExternalStoragePermission()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                permissionToRequest.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
-            } else {
-                permissionToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            }
-        }
-
-        if (!hasReadExternalStoragePermission()) {
-            permissionToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-        }
-
-        if (permissionToRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                this,
-                permissionToRequest.toTypedArray(),
-                0
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0 && grantResults.isNotEmpty()) {
-            for (i in grantResults.indices) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("PermissionsRequest", "${permissions[i]} granted.")
-                }
-            }
-        }
     }
 
     override fun onClick(view: View) {
---
+++ app/src/main/java/com/example/videoapp/view/activities/SplashActivity.kt
@@ -0,0 +1,91 @@
+package com.example.videoapp.view.activities
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.videoapp.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        requestPermissions()
+    }
+
+    private fun hasReadExternalStoragePermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasManageOrWriteExternalStoragePermission() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.MANAGE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+    private fun requestPermissions() {
+        val permissionToRequest = mutableListOf<String>()
+
+        if (!hasManageOrWriteExternalStoragePermission()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                permissionToRequest.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+            } else {
+                permissionToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }
+
+        if (!hasReadExternalStoragePermission()) {
+            permissionToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        if (permissionToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionToRequest.toTypedArray(),
+                0
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PermissionsRequest", "${permissions[i]} granted.")
+                    splashTransaction()
+                }
+            }
+        }
+    }
+
+    private fun splashTransaction() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, SignInWithActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
